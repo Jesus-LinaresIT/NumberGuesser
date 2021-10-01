90b4d626d15584f3bb3cfccdd2d8e17a68f508ed
JesusLinares01 <alberto.salinas@asesoriait.com>
2021-10-01 14:35:24 -0600

    Create functions lose case and game over

--- js/app.js
+++ js/app.js
@@ -28,29 +28,54 @@ maxNum.textContent = max;
 // Liste for guess
 guessBtn.addEventListener('click', ()=>{
    let guess = parseInt(guessInput.value);
+   let assignCls;
    // Validate
-   if(isNaN(guess) || guess < min || guess > max){
-      let msg = `Please enter a number between ${min} and ${max}`;
-      assignCls = 'alert alert-danger';
-      setMessage(msg, assignCls);
-   }
-
-   // Check if won
-   if(guess !== winningNum){
+   if(guess < min || guess > max || isNaN(guess)){
       let msg = `Please enter a number between ${min} and ${max}`;
       assignCls = 'alert alert-danger';
       setMessage(msg, assignCls);
    }else{
-      // Disable input
+      // Check if won
+      if(guess !== winningNum){
+         // Wrong number
+         guessesLeft -= 1;
+
+         if(guessesLeft === 0){
+            // Game Over - lost
+            let msg = `Game over, you lost. The correct number was ${winningNum}`;
+            gameOver(false, msg);
+         }else{
+            // Game continues - answer wrong
+            let assignCls = 'alert alert-warning';
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, assignCls);
+         }
+      }else{
+         // Game Over - won
+         let msg = `Congratulation ${winningNum} is a number correct!`;
+         gameOver(true, msg)
+      }
+   }
+
+});
+
+
+// Game Over
+function gameOver(won, msg){
+   let assignCls;
+   if(won === true){
+      assignCls='alert alert-success';
       guessInput.disabled = true;
       // Change border color
       guessInput.className = "border border-success";
-      // Set Message
-      let msg = `Congratulation ${winningNum} is a number correct!`;
-      assignCls = 'alert alert-success';
+      setMessage(msg, assignCls);
+   }else{
+      assignCls='alert alert-danger';
+      guessInput.disabled = true;
+      // Change border color
+      guessInput.className = "border border-danger";
       setMessage(msg, assignCls);
    }
-});
+}
 
 // Set Message
 function setMessage(msg, assignCls){
